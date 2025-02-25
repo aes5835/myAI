3998cc70340ab504c34bb239356b099d0efcab01
aes5835 <aestrong@hotmail.com>
2025-02-25 11:30:47 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Pharmacy AI TA`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const FOOTER_MESSAGE: string = `All Information Obtainted through Dipiro Text Book and Current Published Phamaracy Practice Guidelines`;
+export const CLEAR_BUTTON_TEXT: string = `Erase Current Chat`;
+export const PAGE_TITLE: string = `Study Pharmacotherapy Effectivley and Correctly`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s Custom Pharmacotherapy TA AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
