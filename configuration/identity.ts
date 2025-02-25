e3defc1a4f7b6dc9ee6805f8fd5f50ba95bcb5a6
aes5835 <aestrong@hotmail.com>
2025-02-25 11:24:30 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Alec Strong`;
+export const OWNER_DESCRIPTION: string = `Alec is a PharmD/MBA student at UNC`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
